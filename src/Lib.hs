11cb0c47e668a5cb0e922c3914d15e001f63cc43
CaesiumFox <avdeev20020904@gmail.com>
2025-04-01 15:18:14 +0300

    Add exitFailure

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,7 +8,7 @@ import System.Exit(exitFailure)
 import Stella.Abs(Program)
 import Stella.Lex(Token, mkPosToken)
 import Stella.Par(pProgram, myLexer)
-import Stella.Print(printTree)
+--import Stella.Print(printTree)
 import Stella.Skel()
 
 import Checker
@@ -17,7 +17,9 @@ type Err        = Either String
 type ParseFun a = [Token] -> Err a
 
 runFile :: ParseFun Program -> FilePath -> IO ()
-runFile p f = putStrLn f >> readFile f >>= run p
+runFile p f = do
+    --putStrLn f
+    readFile f >>= run p
 
 run :: ParseFun Program -> String -> IO ()
 run p s =
@@ -29,21 +31,25 @@ run p s =
       putStrLn err
       exitFailure
     Right tree -> do
-      putStrLn "\nParse Successful!"
-      showTree tree
+      --putStrLn "\nParse Successful!"
+      --showTree tree
       runChecker tree
   where
   ts = myLexer s
   showPosToken ((l,c),t) = concat [ show l, ":", show c, "\t", show t ]
 
+{-
 showTree :: Program -> IO ()
 showTree tree = do
   putStrLn $ "\n[Abstract Syntax]\n\n" ++ show tree
   putStrLn $ "\n[Linearized tree]\n\n" ++ printTree tree
+-}
 
 runChecker :: Program -> IO ()
 runChecker tree = case checkProgram tree of
-    Left err -> putStrLn $ show err
+    Left err -> do
+        putStr $ show err
+        exitFailure
     Right _ -> return ()
 
 someFunc :: IO ()
